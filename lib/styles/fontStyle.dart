76ad98e44a11f048900d963c0275b8b21d8e44c5
umemotokei <keikeikun@icloud.com>
2022-08-21 01:22:00 +0900

    icon_and_screen

---
+++ lib/styles/fontStyle.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const TitleFont = "Billabong";
+const RegularFont = "BIZUDGothic-Regular";
+const BoldFont = "BIZUDGothic-Bold";
+const NovelKleeRFont = "KleeOne-Regular";
+const NovelSararaRFont = "SararaR";
+const NovelYomogiFont = "Yomogi-Regular";
